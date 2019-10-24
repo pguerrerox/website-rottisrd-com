27f4fe90dfd6d5e79385642365ae9ccdaa7d811a
Pedro Guerrero <pguerrerox@users.noreply.github.com>
2019-10-24 17:18:06 -0400

    ...

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <div class="header-content">
+      <img src="" alt="">
+      <nav class="nav">
+        <g-link class="nav-link" to="/">Inicio</g-link>
+        <g-link class="nav-link" to="/sobre-nosotros">Sobre Nosotros</g-link>
+        <g-link class="nav-link" to="/nuestra-gente">Nuestra Gente</g-link>
+        <g-link class="nav-link" to="/nuestros-productos">Nuestros Productos</g-link>
+        <g-link class="nav-link" to="/contactos">Contactos</g-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  
+  }
+</script>
+  
+<style scoped>
+  header{
+    width: 100%;
+    height: 30px;
+    background-color: red
+  }
+
+</style>
